09907aced11a222f8de419c2154ecaaefca2b867
yang69 <yang66999@qq.com>
2017-04-15 21:47:47 +0800

    从尾到头打印链表：将ListNode定义为静态内部类

--- src/PrintListFromTailToHead.java
+++ src/PrintListFromTailToHead.java
@@ -5,6 +5,21 @@ import java.util.ArrayList;
  * 输入一个链表，从尾到头打印链表每个节点的值。
  */
 public class PrintListFromTailToHead {
+    private static class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+        public String toString() {
+            if(next == null) {
+                return val + "";
+            }
+            return val + ", " + this.next;
+        }
+    }
+
     public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
         ArrayList<Integer> res = new ArrayList<>();
         while(listNode != null) {
@@ -18,17 +33,7 @@ public class PrintListFromTailToHead {
         ListNode listNode = new ListNode(0);
         listNode.next = new ListNode(1);
         listNode.next.next = new ListNode(2);
+        System.out.println(listNode);
         System.out.println(new PrintListFromTailToHead().printListFromTailToHead(listNode));
     }
 }
-
-
-class ListNode {
-    int val;
-    ListNode next = null;
-
-    ListNode(int val) {
-    this.val = val;
-    }
-}
-
